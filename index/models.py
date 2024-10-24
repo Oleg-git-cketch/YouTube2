f5712d952daf600bb0e926b73dcebee853364a9d
Oleg-git-cketch <aliol439003@gmail.com>
2024-10-24 23:23:37 +0500

    Second commit

--- index/models.py
+++ index/models.py
@@ -1,4 +1,3 @@
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -6,11 +5,10 @@ from django.db import models
 class Video(models.Model):
     video_title = models.CharField(max_length=128)
     video_content = models.ImageField(upload_to='media')
-    video_video = models.FileField(upload_to='videos')
     user = models.TextField()
     like = models.TextField()
     dislike = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField()
 
     def __str__(self):
         return str(self.video_title)
